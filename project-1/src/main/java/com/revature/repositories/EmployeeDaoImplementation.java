df021401a70b51a5f8ac492fe68ba3f7b604f2d2
Alchemist <59194756+Berkeley-gov@users.noreply.github.com>
2021-03-07 12:23:34 -0800

    update to Employee object

--- project-1/src/main/java/com/revature/repositories/EmployeeDaoImplementation.java
+++ project-1/src/main/java/com/revature/repositories/EmployeeDaoImplementation.java
@@ -1,12 +1,12 @@
 package com.revature.repositories;
 
 import com.revature.models.Employee;
+import com.revature.models.Role;
 import com.revature.utility.JDBConnection;
 import org.apache.log4j.Logger;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -27,7 +27,7 @@ public class EmployeeDaoImplementation implements EmployeDao {
             Connection connected = JDBConnection.getConnection();
 
             // columns holds the column names of the Employee table as string values; to be used for SQL query
-            String sqlQuery = "INSERT INTO \"Employee\" (username, pass_word, first_name, last_name, email, user_role_id) VALUES (?, ?, ?, ?, ?, ?)";
+            String sqlQuery = "INSERT INTO \"Employee\" username pass_word, first_name, last_name, email, user_role_id) VALUES (?, ?, ?, ?, ?, ?)";
 
             sqlStatement = connected.prepareStatement(sqlQuery);
 
@@ -40,6 +40,10 @@ public class EmployeeDaoImplementation implements EmployeDao {
 
             sqlStatement.execute();
 
+            log.info("Successful: The Employee was successfully added to the ERS database.");
+            connected.close();
+            sqlStatement.close();
+
         } catch (SQLException e) {
             log.error("WARN: Failed to execute insert employee query to the ERS database.");
             e.printStackTrace();
@@ -48,19 +52,104 @@ public class EmployeeDaoImplementation implements EmployeDao {
         return true;
     }
 
+    /**
+     * update(): When invoked, update will persist all updated employee information to the
+     * database. Updated information allowed to an employee record are username, password, first name,
+     * last name, and email.
+     * @param employee
+     * @return boolean
+     */
     @Override
     public boolean update(Employee employee) {
-        return false;
+        PreparedStatement sqlStatement;
+
+        try {
+
+            Connection connection = JDBConnection.getConnection();
+
+            String sqlQuery = "UPDATE Employee SET username = ?, pass_word = ?, first_name = ?, last_name = ?, email = ? WHERE employee_id = " + employee.getId() + "";
+
+            sqlStatement = connection.prepareStatement(sqlQuery);
+
+            sqlStatement.setString(1, employee.getUsername());
+            sqlStatement.setString(2, employee.getPassword());
+            sqlStatement.setString(3, employee.getFirstName());
+            sqlStatement.setString(4, employee.getLastName());
+            sqlStatement.setString(5, employee.getEmail());
+
+            log.info("Successful: Employee information was successfully update to the ERS database.");
+            sqlStatement.executeUpdate();
+
+        } catch (SQLException e) {
+            log.warn("WARN: Failed to query the employee updated information to the the ERS database");
+            e.printStackTrace();
+            return false;
+        }
+        return true;
     }
 
     @Override
     public boolean delete(Employee employee) {
-        return false;
+        PreparedStatement sqlStatement;
+
+        try {
+
+            Connection connection = JDBConnection.getConnection();
+
+            String sqlQuery = "DELETE FROM Employee WHERE employee_id = " + employee.getId() + "";
+            sqlStatement = connection.prepareStatement(sqlQuery);
+            sqlStatement.executeQuery();
+
+            log.info("Successful: Employee: " + employee.getFirstName() + "was successfully deleted from the ERS database");
+            connection.close();
+            sqlStatement.close();
+
+        } catch (SQLException e) {
+            log.warn("WARN: Failed to delete the employee from the ERS database.");
+            e.printStackTrace();
+            return false;
+        }
+
+        return true;
     }
 
     @Override
     public List<Employee> findAll() {
-        return null;
+        List<Employee> allEmployees = new ArrayList<Employee>();
+
+        try {
+
+            String sqlQuery = "SELECT * FROM Employee INNER JOIN Role ON Employee.employee_id = Role.role_id";
+            Connection connection = JDBConnection.getConnection();
+            Statement sqlStatement = connection.createStatement();
+            ResultSet sqlResults = sqlStatement.executeQuery(sqlQuery);
+
+            while (sqlResults.next()) {
+                int employeeId = sqlResults.getInt("employee_id");
+                String username = sqlResults.getString("username");
+                String password = sqlResults.getString("pass_word");
+                String firstName = sqlResults.getString("first_name");
+                String lastName = sqlResults.getString("last_name");
+                String email = sqlResults.getString("email");
+                int roleId = sqlResults.getInt("role_id");
+                String employeeRole = sqlResults.getString("user_role");
+
+                Role role = new Role(roleId, employeeRole);
+                Employee employee = new Employee(employeeId, username, password, firstName, lastName, email, role);
+                allEmployees.add(employee);
+            }
+
+            log.info("Successful: All Employee records were successfully retrieved from the ERS database");
+            connection.close();
+            sqlStatement.close();
+            sqlResults.close();
+        }
+        catch (SQLException e) {
+            log.warn("WARN: Failed to retrieve all the employees from the ERS database.");
+            e.printStackTrace();
+            return null;
+
+        }
+        return allEmployees;
     }
-    //TODO finsh building the Employee Dao layer
 }
